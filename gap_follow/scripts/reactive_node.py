65cb226c31826ac109409bba22ed7aebc5abf7e2
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2025-02-14 21:45:11 -0500

    Some improvements

--- gap_follow/scripts/reactive_node.py
+++ gap_follow/scripts/reactive_node.py
@@ -6,6 +6,7 @@ import numpy as np
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 
+
 class ReactiveFollowGap(Node):
     """ 
     Implement Wall Following on the car
@@ -30,8 +31,10 @@ class ReactiveFollowGap(Node):
             10
         )
 
-        self.max_dist = 5.0 # meters
-        self.bubble_width = 0.50 # meters
+        self.moving_avg_window = 3
+        self.max_dist = 10.0 # meters
+        self.bubble_radius = 20 # indices in ranges array
+        self.min_obs_dist_threshold = 2.5 # meters
 
         self.front_view_start_idx = None
         self.front_view_end_idx = None
@@ -54,36 +57,40 @@ class ReactiveFollowGap(Node):
     def steering_angle_to_velocity_mapping(self, angle):
         """ Simple mapping between steering angle and velocity
         """
-        return 1.0
-
+        # Convert steering angle to safe speed
+        curr_velocity = 3.0
+        if np.abs(angle) < np.radians(10):
+            curr_velocity = 3.0
+        elif np.abs(angle) < np.radians(20):
+            curr_velocity = 2.0
+        else:
+            curr_velocity = 1.0
+        return curr_velocity
+    
+    
+    def lidar_to_steering_angle(self, lidar_angle):
+        """ Convert a LiDAR angle to a steering angle
+        """
+        # As need to map lidar angle from -90 to 90 to steering angle from -45 to 45.
+        return lidar_angle / 2.0
 
     def preprocess_lidar(self, ranges):
         """ Preprocess the LiDAR scan array. Expert implementation includes:
             1.Setting each value to the mean over some window
             2.Rejecting high values (eg. > 3m)
         """
-        proc_ranges = ranges
-        # Convert nan values to zero, and inf values to max value
+        # Create a copy of the ranges array as a numpy array
+        proc_ranges = np.array(ranges)
+        # Convert nan values to zero, and higher than max_dist to max_dist
         for i in range(len(proc_ranges)):
             if np.isnan(proc_ranges[i]):
                 proc_ranges[i] = 0.0
-            elif np.isinf(proc_ranges[i]):
+            elif np.isinf(proc_ranges[i]) or proc_ranges[i] > self.max_dist:
                 proc_ranges[i] = self.max_dist
 
-        # Apply a moving average filter
-        window_size = 5
-        for i in range(len(proc_ranges)):
-            if i < window_size:
-                proc_ranges[i] = np.mean(ranges[:i+window_size])
-            elif i > len(proc_ranges) - window_size:
-                proc_ranges[i] = np.mean(ranges[i-window_size:])
-            else:
-                proc_ranges[i] = np.mean(ranges[i-window_size:i+window_size])
-
-        # Reject high values
-        for i in range(len(proc_ranges)):
-            if proc_ranges[i] > self.max_dist:
-                proc_ranges[i] = self.max_dist
+        # Apply a moving average filter using np.convolve()
+        kernel = np.ones(self.moving_avg_window) / self.moving_avg_window
+        proc_ranges = np.convolve(proc_ranges, kernel, mode='same')
 
         return proc_ranges
 
@@ -91,20 +98,25 @@ class ReactiveFollowGap(Node):
         """
         Return the start index & end index of the max gap in free_space_ranges
         """
+        max_gap = 0
         start_i = 0
         end_i = 0
-        max_gap = 0
         gap = 0
-        threshold = 4
-        for i in range(self.front_view_start_idx, self.front_view_end_idx):
-            if free_space_ranges[i] > threshold:
+        for i in range(len(free_space_ranges)):
+            if free_space_ranges[i] > self.min_obs_dist_threshold:
                 gap += 1
             else:
                 if gap > max_gap:
                     max_gap = gap
-                    end_i = i
                     start_i = i - gap
+                    end_i = i
                 gap = 0
+
+        # Check the last gap
+        if gap > max_gap:
+            start_i = len(free_space_ranges) - gap
+            end_i = len(free_space_ranges) - 1
+
         return start_i, end_i
 
 
@@ -113,11 +125,7 @@ class ReactiveFollowGap(Node):
         Return index of best point in ranges
 	    Naive: Choose the furthest point within ranges and go there
         """
-        best_index = start_i
-        for i in range(start_i, end_i):
-            if ranges[i] > ranges[best_index]:
-                best_index = i
-        return best_index
+        return np.argmax(ranges[start_i:end_i+1]) + start_i
 
 
     def lidar_callback(self, data):
@@ -129,40 +137,35 @@ class ReactiveFollowGap(Node):
         self.front_view_start_idx = self.angle_to_index(-np.pi/2, data)
         self.front_view_end_idx = self.angle_to_index(np.pi/2, data)
         
-        # TODO:
-        #Find closest point to LiDAR - minimum non-zero value
-        # Only to select from -90 to 90 degrees
-        min_index = -1
-        min_value = self.max_dist
+        # Find closest point to LiDAR - minimum non-zero value
+        min_index = self.front_view_start_idx
+        min_value = proc_ranges[min_index]
         for i in range(self.front_view_start_idx, self.front_view_end_idx):
             if proc_ranges[i] < min_value and proc_ranges[i] > 0:
                 min_value = proc_ranges[i]
                 min_index = i
 
-        # Eliminate all points inside 'bubble' (set them to zero) 
-        # Choose bubble size, such that 2*r*theta is approx car width
-        # r is the distance to the closest point.
-        theta_bubble = self.bubble_width / (2 * min_value)
-        bubble_size = int(theta_bubble / data.angle_increment)
-        for i in range(min_index-bubble_size, min_index+bubble_size):
-            if i >= 0 and i < len(proc_ranges):
-                proc_ranges[i] = 0.0
-
-        #Find max length gap)
-        start_i, end_i = self.find_max_gap(proc_ranges)
-        self.range_index_to_angle(end_i, data)
-
-        #Find the best point in the gap
-        best_index = self.find_best_point(start_i, end_i, proc_ranges)
-
-        # Choose steering angle and velocity
-        angle = self.range_index_to_angle(best_index, data)
+        angle = 0.0
         velocity = self.steering_angle_to_velocity_mapping(angle)
+        if min_value != self.max_dist:
+            for i in range(min_index-self.bubble_radius, min_index+self.bubble_radius):
+                if i >= 0 and i < len(proc_ranges):
+                    proc_ranges[i] = 0.0
+
+            #Find max length gap
+            start_i, end_i = self.find_max_gap(proc_ranges)
+
+            #Find the best point in the gap
+            best_index = self.find_best_point(start_i, end_i, proc_ranges)
+
+            # Choose steering angle and velocity
+            angle = self.range_index_to_angle(best_index, data)
+            velocity = self.steering_angle_to_velocity_mapping(angle)
 
         #Publish Drive message
         drive_msg = AckermannDriveStamped()
         drive_msg.header = data.header
-        drive_msg.drive.steering_angle = angle
+        drive_msg.drive.steering_angle = self.lidar_to_steering_angle(angle)
         drive_msg.drive.speed = velocity
         self.drive_pub.publish(drive_msg)
 
